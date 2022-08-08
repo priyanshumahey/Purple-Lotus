347e6d20e62e01d282f4fc4c19852df3c6e9a6da
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2022-08-08 13:22:18 -0700

    Homepage updates

--- site/src/Pages/Intro/App.js
+++ site/src/Pages/Intro/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function Intro() {
   return (
     <div className="App">
+      Intro Page
     </div>
   );
 }
--- site/src/index.js
+++ site/src/index.js
@@ -2,28 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Intro from './Pages/Intro/App';
+import Homepage from './Pages/Home/Home';
 import reportWebVitals from './reportWebVitals';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 // Project Organization
 // - Intro Screen
-//      -> Login
-//      -> Set-up
 // - Homepage
-// - Settings Page
+// - Settings Page ??
 // - Parent Page
 // - Child Page
 // - Therapist Page
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Intro />} />
-      {/* <Route path="/SignUp" element={<SignUp />} /> */}
-      {/* <Route path="/Login" element={<Login />} /> */}
       {/* <Route path="/Settings" element={<Settings />} /> */}
-      {/* <Route path="/Home" element={<Homepage />} /> */}
+      <Route path="/Home" element={<Homepage />} />
       {/* <Route path="/ForParents" element={<Parents />} /> */}
       {/* <Route path="/ForChild" element={<Child />} /> */}
       {/* <Route path="/ForTherapist" element={<Therapist />} /> */}
