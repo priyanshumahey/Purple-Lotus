1f1b2cb833638767f9d067fd4a23e05a3de3ca96
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2022-08-17 10:53:52 -0700

    Updated therapists side

--- site/src/Pages/Therapy/Therapist.js
+++ site/src/Pages/Therapy/Therapist.js
@@ -1,6 +1,26 @@
+import './therap.css'
+
 function Therapy () {
     return(
-        <h1>Therapy</h1>
+        <div className='app'>
+            <h1>For Therapists</h1>
+            <h2>Preface</h2>
+            <p>These modules are still early in development! 
+                This current build is primarily for demonstration purposes and proof of concept!
+                The current build is a stable build missing core features which will be present further on
+                down the line. For now, feel free to explore the current version and enjoy the demo!
+                The demo version only demonstrates Chinese Canadian youth and only has support for English
+                and Mandarin. Future versions will include further language support, further culture support
+                as well as more accessibility options and continued support for various groups.
+            </p>
+            <h2>Overview</h2>
+            <p>
+                This module would go over the issues of both parents and children and attempt to explain it with
+                a therapy centered focus. The goal would be to teach therapists who may not have much knowledge
+                about Asian cultures and help them gain a deeper understanding of the cultural aspects of different 
+                Asian culutres. 
+            </p>
+        </div>
     )
 }
 
